2f5a7cac6bb1f2a756b9c0178a4d502944f294b4
elliott hebert <elliotthebert@MacBook-Pro-de-elliott.local>
2015-11-27 13:57:15 +0100

    attributs gates

--- areoport/gates.java
+++ areoport/gates.java
@@ -5,10 +5,37 @@
  */
 package areoport;
 
+import static areoport.TypeGates.*;
+
 /**
  *
  * @author elliotthebert
  */
+ enum TypeGates{
+     SMALL("porte national"),
+     REGULAR("petite porte international"),
+     BIG("grande porte international")
+     ;
+     
+     private String info;
+     TypeGates(String info){
+         this.info = info;
+     }
+}
 public class gates {
+    private int number;
+    private boolean libre;
+    private TypeGates type;
     
+    public gates(){
+        number = 0;
+        libre = true;
+        type = SMALL;
+    }
+    
+    public gates(int number, TypeGates type){
+        this.number = number;
+        this.libre = true;
+        this.type = type;
+    }
 }
